9ee7db4c91da9d50eec2463d0b812e44fbee6242
Yuji T <accmanager1990@gmail.com>
2022-05-12 04:39:05 +0900

    Implement webpack w/ jekyll

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', './app.js'),
+  entry: path.resolve(__dirname, './app.js'),
   output: {
-    path: path.resolve(__dirname, 'src', 'dist'),
+    path: path.resolve(__dirname, 'assets', 'js'),
     filename: 'main.js',
     clean: true,
   },
